1c240a54447cbed88c6ebf8acca7b5c7d994c50d
Jaeden Lebron <jaeden.lebron@vtsd.com>
2022-01-18 11:52:28 -0500

    challenge

--- min-Challenge/challenge.js
+++ min-Challenge/challenge.js
@@ -2,5 +2,6 @@
     Design a page that when you change a text input, and image gets bigger
  */
 function imgbig(){
-
+    var img = document.getElementById("img");
+    img.style.width = "200%";
 }
